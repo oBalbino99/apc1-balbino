5e91eaad594996e8401f70da5f8f64475403d8ee
Rafael Balbino <obalbino312@gmail.com>
2024-08-22 00:57:35 +0000

    praticas02

---
+++ praticas/pratica02/imprima_fibonacci.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+  printf("%12i, %i\n", 1,1);
+  printf("%10i, %i, %i\n",1,2,1);
+  printf("%7i ,%i, %i, %i, %i\n", 1,2,3,2,1);
+  printf("%4i, %i, %i, %i, %i, %i, %i\n", 1,2,3,5,3,2,1);
+  printf("%i, %i, %i, %i, %i, %i, %i, %i, %i\n", 1,2,3,5,8,5,3,2,1);
+  return 0;
+}
